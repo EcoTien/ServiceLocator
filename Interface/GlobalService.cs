4ed8e652f2e3b4ede2ea2c02774b645b8fe70642
Tien Company <tiendv@blackgemsglobal.com>
2025-01-10 14:42:47 +0700

    fix global service

--- Interface/GlobalService.cs
+++ Interface/GlobalService.cs
@@ -5,14 +5,18 @@ namespace EcoMine.Service
 {
     public abstract class GlobalService<T> : MonoBehaviour, IService where T : class, IService
     {
+        private bool _isRegister;
+        
         void IService.RegisterService()
         {
+            if(_isRegister) return;
             if (ServiceLocator.IsRegistered<T>(this))
             {
                 Destroy(this);
             }
             else
             {
+                _isRegister = true;
                 DontDestroyOnLoad(this);
                 ServiceLocator.RegisterGlobalService(this, this as T);
             }
