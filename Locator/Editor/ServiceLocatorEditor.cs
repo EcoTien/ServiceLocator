8fbccbca10e67a466f13b1afdc00e644f817ebb1
Tien Company <tiendv@blackgemsglobal.com>
2025-02-04 15:42:11 +0700

    update service locator editor: fix edit scene

--- Locator/Editor/ServiceLocatorEditor.cs
+++ Locator/Editor/ServiceLocatorEditor.cs
@@ -1,65 +1,61 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 namespace EcoMine.Service.Editor
 {
-    /*[InitializeOnLoad]*/
+    [InitializeOnLoad]
     internal sealed class ServiceLocatorEditor
     {
         private static string currentScenePath;
         static ServiceLocatorEditor()
         {
             EditorApplication.playModeStateChanged += PlayModeStateChanged;
-            
+            EditorApplication.hierarchyChanged += HierarchyChanged;
             if(EditorApplication.isPlayingOrWillChangePlaymode) return;
-            
             currentScenePath = SceneManager.GetActiveScene().path;
-            EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
-            bool needInitialize = false;
-            
-            for (int i = 0; i < scenes.Length; i++)
-            {
-                string scenePath = scenes[i].path;
-                
-                Scene scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
-                if (IsHasComponent<ServiceLocatorRuntime>(scene)) continue;
-                needInitialize = true;
-            }
-            if (needInitialize)
-            {
-                bool userChoice = EditorUtility.DisplayDialog(
-                    "Service Locator Initialize", 
-                    "Do you want to initialize the service locator runtime?", 
-                    "Yes", "No");
+            InitializeServiceLocatorRuntime();
+        }
 
-                if (userChoice) InitializeServiceLocatorRuntime();
-                else OpenPersionalScene();
-            }
-            else
-            {
-                OpenPersionalScene();
-            }
+        private static void HierarchyChanged()
+        {
+            string scenePath = SceneManager.GetActiveScene().path;
+            if (CurrentSceneHasIService() && !CurrentSceneHasServiceLocatorRuntime())
+                OpenSceneAndCreateServiceLocatorRuntime(scenePath, false);
         }
 
         private static void InitializeServiceLocatorRuntime()
         {
-            EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
-            for (int i = 0; i < scenes.Length; i++)
+            string[] guids = AssetDatabase.FindAssets("t:Scene");
+            bool isChangeScene = false;
+            for (var i = 0; i < guids.Length; i++)
             {
-                string scenePath = scenes[i].path;
-                
-                Scene scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
-                if (IsHasComponent<ServiceLocatorRuntime>(scene)) continue;
-                
-                PrefabUtility.InstantiatePrefab(Resources.Load<ServiceLocatorRuntime>("ServiceLocatorRuntime"));
-                EditorSceneManager.SaveScene(scene);
+                string scenePath = AssetDatabase.GUIDToAssetPath(guids[i]);
+                if (HasIService(scenePath) && !HasPrefabServiceLocatorRuntime(scenePath))
+                {
+                    isChangeScene = true;
+                    OpenSceneAndCreateServiceLocatorRuntime(scenePath);
+                }
             }
-            OpenPersionalScene();
+            if(isChangeScene) OpenPersonalScene();
+        }
+
+        private static void OpenSceneAndCreateServiceLocatorRuntime(string scenePath, bool loadScene = true)
+        {
+            Scene scene = default;
+            if(loadScene) scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+            PrefabUtility.InstantiatePrefab(Resources.Load<ServiceLocatorRuntime>("ServiceLocatorRuntime"));
+            if(loadScene) EditorSceneManager.SaveScene(scene);
         }
         
-        private static void OpenPersionalScene()
+        private static void OpenPersonalScene()
         {
             if (!SceneManager.GetActiveScene().path.Equals(currentScenePath))
                 EditorSceneManager.OpenScene(currentScenePath, OpenSceneMode.Single);
@@ -70,17 +66,55 @@ namespace EcoMine.Service.Editor
             if(playModeStateChange == PlayModeStateChange.ExitingPlayMode)
                 ServiceLocator.UnregisterAllService();
         }
-
-        private static bool IsHasComponent<T>(Scene scene)
+        
+        private static bool HasPrefabServiceLocatorRuntime(string scenePath)
         {
-            GameObject[] rootGameObjects = scene.GetRootGameObjects();
-            for (int i = 0; i < rootGameObjects.Length; i++)
-            {
-                T component = rootGameObjects[i].GetComponentInChildren<T>();
-                if (component != null)
-                    return true;
-            }
-            return false;
+            string[] lines = File.ReadAllLines(scenePath);
+            string guid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(Resources.Load("ServiceLocatorRuntime")));
+            return lines
+                .Where(HasM_SourcePrefab)
+                .Select(GetGuid)
+                .Any(prefabGuid => prefabGuid.Equals(guid));
+        }
+        
+        private static bool HasIService(string scenePath)
+        {
+            string[] lines = File.ReadAllLines(scenePath);
+            return lines
+                .Where(HasM_Script)
+                .Select(GetGuid)
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<MonoScript>)
+                .Any(script => script != null && script.GetClass() != null && typeof(IService).IsAssignableFrom(script.GetClass()));
+        }
+
+        private static bool CurrentSceneHasIService()
+        {
+            return Object.FindObjectsOfType<MonoBehaviour>().OfType<IService>().Any();
+        }
+
+        private static bool CurrentSceneHasServiceLocatorRuntime()
+        {
+            return Object.FindObjectOfType<ServiceLocatorRuntime>() != null;
+        }
+
+        private static bool HasM_Script(string line)
+        {
+            return line.Contains("m_Script");
+        }
+
+        private static bool HasM_SourcePrefab(string line)
+        {
+            return line.Contains("m_SourcePrefab");
+        }
+
+        private static string GetGuid(string guid)
+        {
+            string pattern = @"guid:\s([a-f0-9]{32})";
+            Regex regex = new Regex(pattern);
+            
+            Match match = regex.Match(guid);
+            return match.Groups[1].Value;
         }
     }
 }
